f5c221cff0ba133e16077e1a8bc6ae01f29b27e7
Kristian Røste <github@roste.org>
2024-01-24 09:32:28 +0100

    Refactor dashboard.php to include a disclaimer and improve header layout

--- modules/!dashboard.php
+++ modules/!dashboard.php
@@ -1,11 +1,21 @@
 <div id="dashboard" class="content">
 <div class="card">
-  <h1 class="card-header">Welcome to RAND!</h1>
+  <h1 class="card-header d-flex justify-content-between">
+    <span>Welcome to RAND!</span>
+  </h1>
   <div class="card-body">
     <p class="text-muted">
+      <h1 class="text-primary">RAND</h1>
       This page includes a bunch of tools. Choose a tool above to get started.
       <br>
       To see more, click one of the links below or in the navbar.
+
+      <hr>
+
+      <h5 class="text-danger"><?= icon("exclamation-triangle-fill") ?> Disclaimer</h5>
+      <p>
+        RAND is by no means a professional tool. It is a tool that I made for myself, and I am sharing it with you.
+        The code is not perfect, and there are probably bugs. If you find a bug, please report it on the GitHub page.
     </p>
 
     <hr>
