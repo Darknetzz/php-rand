4a10d68b7037fd561baca7529494c0ce814ec872
Kristian <github@roste.org>
2025-08-09 13:00:46 +0200

    check for logogen presence

--- modules/gen_image.php
+++ modules/gen_image.php
@@ -3,9 +3,20 @@
     <div class="card card-primary">
         <h1 class="card-header"><?= icon("image", 1, 2) ?> Image Generator</h1>
         <div class="card-body">
-            <a href="php-logogen" target="_blank" class="btn btn-primary mb-3">
-                <i class="fa fa-arrow-left"></i> Open Image Generator
-            </a>
+            <?php
+                do {
+                    $logoGenUrl = SITE_BASE_URL . "/" . "php-logogen";
+                    if (!is_dir($logoGenUrl)) {
+                        echo "<span class='description'>Logogen not found ($logoGenUrl)</span>";
+                        break;
+                    }
+                    echo '
+                    <a href="'.$logoGenUrl.'" target="_blank" class="btn btn-primary mb-3">
+                        <i class="fa fa-arrow-left"></i> Open Image Generator
+                    </a>
+                    ';
+                } while (False);
+            ?>
         </div>
     </div>
 
