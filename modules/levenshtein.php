3fada15b49b38c682864fdb163b9c14eee295521
Kristian <github@roste.org>
2025-10-23 13:39:37 +0200

    levenshtein: added some padding between labels and inputs

--- modules/levenshtein.php
+++ modules/levenshtein.php
@@ -8,13 +8,28 @@
                 The complexity of the algorithm is O(m*n), where n and m are the length of string1 and string2 (rather good when compared to similar_text(), which is O(max(n,m)**3), but still expensive).
             </span>
             <form class="form" action="gen.php" method="POST" id="levenshtein" data-action="levenshtein">
-                <textarea class="form-control" name="levenshtein1" placeholder="String 1"></textarea>
-                <textarea class="form-control" name="levenshtein2" placeholder="String 2"></textarea>
-
-                Insertion cost: <input type="number" class="form-control" name="insertion_cost" placeholder="Insertion cost (default: 1)">
-                Replacement cost: <input type="number" class="form-control" name="replacement_cost" placeholder="Replacement cost (default: 1)">
-                Deletion cost: <input type="number" class="form-control" name="deletion_cost" placeholder="Deletion cost (default: 1)">
-                
+                <table class="form-table">
+                    <tr>
+                        <th style="padding-right: 0.75rem;"><label for="levenshtein1">String 1</label></th>
+                        <td><textarea class="form-control" name="levenshtein1" id="levenshtein1" placeholder="String 1"></textarea></td>
+                    </tr>
+                    <tr>
+                        <th style="padding-right: 0.75rem;"><label for="levenshtein2">String 2</label></th>
+                        <td><textarea class="form-control" name="levenshtein2" id="levenshtein2" placeholder="String 2"></textarea></td>
+                    </tr>
+                    <tr>
+                        <th style="padding-right: 0.75rem;"><label for="insertion-cost">Insertion cost</label></th>
+                        <td><input type="number" class="form-control" name="insertion_cost" id="insertion-cost" placeholder="Insertion cost (default: 1)"></td>
+                    </tr>
+                    <tr>
+                        <th style="padding-right: 0.75rem;"><label for="replacement-cost">Replacement cost</label></th>
+                        <td><input type="number" class="form-control" name="replacement_cost" id="replacement-cost" placeholder="Replacement cost (default: 1)"></td>
+                    </tr>
+                    <tr>
+                        <th style="padding-right: 0.75rem;"><label for="deletion-cost">Deletion cost</label></th>
+                        <td><input type="number" class="form-control" name="deletion_cost" id="deletion-cost" placeholder="Deletion cost (default: 1)"></td>
+                    </tr>
+                </table>
                 <?= submitBtn("levenshtein", "action", "Convert", "arrow-repeat") ?>
                 <div class="responseDiv" id="levenshteinresponse"></div>
             </form>
